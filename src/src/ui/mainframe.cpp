f063e4d76fe965f434e1a3cce83167e8987cff72
denglinglong <denglinglong@uniontech.com>
2020-05-19 16:27:21 +0800

    aria2 修改了接口和rpc返回的信号

--- src/src/ui/mainframe.cpp
+++ src/src/ui/mainframe.cpp
@@ -1,6 +1,9 @@
 #include "mainframe.h"
+#include "aria2rpcinterface.h"
 #include <QAction>
 #include <DMenu>
+
+
 MainFrame::MainFrame(QWidget *parent) :
     DMainWindow(parent)
 {
@@ -11,4 +14,23 @@ MainFrame::~MainFrame()
 {
 
 }
+
+//初始化aria2
+void MainFrame::initAria2()
+{
+    //aria2c = new Aria2cInterface(this);
+    connect(Aria2RPCInterface::Instance(), SIGNAL(signal_success(QString, QJsonObject)), this, SLOT(slotRpcSuccess(QString, QJsonObject)));
+    connect(Aria2RPCInterface::Instance(), SIGNAL(signal_error(QString, QString, int)), this, SLOT(slotRpcError(QString, QString, int)));
+}
+
+void MainFrame::slotRPCSuccess(QString method, QJsonObject json)
+{
+
+}
+
+void MainFrame::slotRPCError(QString method, QString id, int error)
+{
+
+}
+
 
