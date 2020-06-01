4ce0cef93b8a51c0ad3a4ad07e6595060c356abd
bulongwei <bulongwei@uniontech.com>
2020-06-01 11:24:53 +0800

    添加提示框

---
+++ src/include/ui/messagebox.h
@@ -0,0 +1,16 @@
+#ifndef MESSAGEBOX_H
+#define MESSAGEBOX_H
+
+
+class MessageBox : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MessageBox(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // MESSAGEBOX_H
