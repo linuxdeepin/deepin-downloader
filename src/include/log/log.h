a9bcf4bdee54ae675ba3664423593a4082744583
denglinglong <denglinglong@uniontech.com>
2020-06-04 16:29:43 +0800

    添加了log模块的注释

--- src/include/log/log.h
+++ src/include/log/log.h
@@ -1,3 +1,12 @@
+/**
+* @file log.h
+* @brief 日志 接口类
+* @author denglinglong  <denglinglong@uniontech.com>
+* @version 1.0.0
+* @date 2020-05-26 11:55
+* @copyright 2020-2020Uniontech Technology Co., Ltd.
+*/
+
 #ifndef LOG_H
 #define LOG_H
 
@@ -22,12 +31,59 @@ static QString s_logPath;
 static QString _logDir;
 static QFile _logFile;
 
+/**
+ *@brief 设置日志目录
+ *@param dir 目录地址
+ *@return
+ */
 void setLogDir(const QString &dir);
+
+/**
+ *@brief 检查磁盘空间是否大于10MB
+ *@param
+ *@return
+ */
 void CheckFreeDisk();
+
+/**
+ *@brief 检查日志时间是否大于7天
+ *@param
+ *@return
+ */
 void CheckLogTime();
+
+/**
+ *@brief 创建新的日志
+ *@param
+ *@return
+ */
 void CreateNewLog();
+
+/**
+ *@brief 检查磁盘大小
+ *@param
+ *@return
+ */
 bool CheckRotateSize();
+
+/**
+ *@brief 设置日志路径
+ *@param path 路径地址
+ *@return
+ */
 void setLogPath(const QString &path);
+
+/**
+ *@brief 设置日志级别
+ *@param level QtDebugMsg;QtWarningMsg;QtCriticalMsg;QtFatalMsg;
+ *@return
+ */
 void setLogLevel(int level);
+
+/**
+ *@brief 用户日志信息
+ *@param
+ *@return
+ */
 void customLogMessageHandler(QtMsgType type, const QMessageLogContext &ctx, const QString &msg);
 #endif // LOG_H
