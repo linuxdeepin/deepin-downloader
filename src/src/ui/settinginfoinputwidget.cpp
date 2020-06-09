87bafe36c16df32f7b26c46d2daefd8825a582f5
yuandandan <yuandandan@uniontech.com>
2020-06-09 13:57:30 +0800

    代码规范化

--- src/src/ui/settinginfoinputwidget.cpp
+++ src/src/ui/settinginfoinputwidget.cpp
@@ -1,11 +1,29 @@
 /**
-* @file settinginfoinputwidget.cpp
-* @brief 设置信息可编辑输入窗口控件，最大下载限速以及最大上传限速用到此控件
-* @author yuandandan  <yuandandan@uniontech.com>
-* @version 1.0.0
-* @date 2020-05-26 14:07
-* @copyright 2020-2020 Uniontech Technology Co., Ltd.
-*/
+ * @copyright 2020-2020 Uniontech Technology Co., Ltd.
+ *
+ * @file settinginfoinputwidget.cpp
+ *
+ * @brief 设置信息可编辑输入窗口控件，最大下载限速以及最大上传限速用到此控件
+ *
+ *@date 2020-06-09 10:45
+ *
+ * Author: yuandandan  <yuandandan@uniontech.com>
+ *
+ * Maintainer: yuandandan  <yuandandan@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "settinginfoinputwidget.h"
 
 #include <QHBoxLayout>
