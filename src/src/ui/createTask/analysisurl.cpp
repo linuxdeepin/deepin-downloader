9f09ad71220eea4f79dbf4f8ed56dc46b9e75757
ut001433 <bulongwei@uniontech.com>
2020-09-28 15:44:37 +0800

    fix: 修改bug
    下载资源确认框--修改下载路径后，获取的下载磁盘大小有误,中英文显示错误
    
    Log: 创建任务时复制下载链接，下载路径的磁盘获取数值显示有误, 复制有效下载链接取消下载后，重新复制更多有效链接，没有实时解析链接, 解析列
    表重命名特殊字符都可以命名， 下载列表点击确认后可以一直点击确认按钮
    
    Bug: https://pms.uniontech.com/zentao/bug-view-49834.html， https://
    pms.uniontech.com/zentao/bug-view-49802.html， https://pms.uniontech.com/
    zentao/bug-view-49789.html， https://pms.uniontech.com/zentao/bug-
    view-49717.html
    
    Change-Id: Ifffeeb46602f46c343d7f3e253cc154f7e79a31b
    Reviewed-on: http://gerrit.uniontech.com/c/beijing/downloadmanager/+/6546
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: denglinglong <denglinglong@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/src/ui/createTask/analysisurl.cpp
+++ src/src/ui/createTask/analysisurl.cpp
@@ -149,7 +149,7 @@ void AnalysisUrl::stopWork(int index)
 
         thread->requestInterruption();
         thread->quit();
-        thread->wait(10);
+        thread->wait(100);
         delete thread;
         delete urlIt.value();
         urlIt.value() = nullptr;
