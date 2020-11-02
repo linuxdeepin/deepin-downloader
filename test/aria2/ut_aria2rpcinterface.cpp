c7187e01f854cadde8df8a52e7d67ea368732d56
zhaoyue <zhaoyue@uniontech.com>
2020-11-02 11:18:21 +0800

    fix: 修改gtest触发形式，添加aria2的单元测试模块
    Change-Id: Ic0c73e0d91c9a54575d9dcff000a138a3855e55d

--- test/aria2/ut_aria2rpcinterface.cpp
+++ test/aria2/ut_aria2rpcinterface.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "aria2rpcinterface.h"
 
+class ut_aria2Test : public ::testing::Test
+{
+protected:
+    ut_aria2Test()
+    {
+    }
+
+    virtual ~ut_aria2Test()
+    {
+    }
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(ut_aria2Test, init)
+{
+    bool ret = Aria2RPCInterface::instance()->init();
+    EXPECT_TRUE(ret);
+}
+
+TEST_F(ut_aria2Test, addUri)
+{
+    QString uri;
+    QMap<QString, QVariant> opt;
+    QString id;
+    bool ret = Aria2RPCInterface::instance()->addUri(uri, opt, id);
+    EXPECT_TRUE(ret);
+}
+
+TEST_F(ut_aria2Test, addTorrent)
+{
+    QString uri;
+    QMap<QString, QVariant> opt;
+    QString id;
+    bool ret = Aria2RPCInterface::instance()->addTorrent(uri, opt, id);
+    EXPECT_TRUE(ret);
+}
+
+TEST_F(ut_aria2Test, addMetalink)
+{
+    QString uri;
+    QMap<QString, QVariant> opt;
+    QString id;
+    bool ret = Aria2RPCInterface::instance()->addMetalink(uri, opt, id);
+    EXPECT_TRUE(ret);
+}
