d9a14fd7da5444586b9fc5bd1532f32adc29d81f
zhaoyue <zhaoyue@uniontech.com>
2020-12-01 14:51:54 +0800

    fix: 修改测试用例
    删除掉QTest，采用GTest，屏蔽界面弹出，使用stub模拟部分接口。
    
    Log: 修改cmakelist，删除掉判断是否为测试模式。
    Change-Id: Ie66e4b35d6b80776e78abf465051ab627c6f47fe

--- test/main.cpp
+++ test/main.cpp
@@ -1,55 +1,13 @@
-
 #include "gtest/gtest.h"
 #include <QApplication>
-#include <QtTest>
-#include "mainframe.h"
 
-class QTestMain;
-
-class QTestMain : public QObject
+int main(int argc, char *argv[])
 {
-    Q_OBJECT
-friend class MainFrame;
-public:
-    QTestMain();
-    ~QTestMain();
+    qputenv("QT_QPA_PLATFORM", "offscreen");
+    QApplication a(argc, argv);
 
-    void testGTest(int argc, char *argv[]);
-private slots:
-    void initTestCase();
-    void cleanupTestCase();
-};
-
-QTestMain::QTestMain()
-{
-}
-
-QTestMain::~QTestMain()
-{
-}
-
-void QTestMain::initTestCase()
-{
-}
-
-void QTestMain::cleanupTestCase()
-{
-}
-
-void QTestMain::testGTest(int argc, char *argv[])
-{
     testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
-    Q_UNUSED(ret)
-}
 
-int main(int argc, char **argv)
-{
-    QApplication a(argc, argv);
-    a.setOrganizationName("uos"); //设置公司名
-    a.setApplicationName("downloader"); //设置应用程序名
-    QTestMain tc;
-    tc.testGTest(argc, argv);
-    return QTest::qExec(&tc, argc, argv);
+    return ret;
 }
-#include "main.moc"
