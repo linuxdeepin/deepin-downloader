b74f78ce629a0d3136d3f685ae0dc7ca3dc1d872
zhaoyue <zhaoyue@uniontech.com>
2020-12-01 14:51:54 +0800

    fix: 修改测试用例
    删除掉QTest，采用GTest，屏蔽界面弹出，使用stub模拟部分接口。
    
    Log: 修改cmakelist，删除掉判断是否为测试模式。
    Change-Id: Ie66e4b35d6b80776e78abf465051ab627c6f47fe

--- test/stubAll.cpp
+++ test/stubAll.cpp
@@ -5,17 +5,37 @@ QAction *QMenu_exec(const QPoint &pos)
     return nullptr;
 }
 
-int DSettingsDialog_exec()
+int DSettingsDialog_exec(void* obj)
 {
     return 0;
 }
 
-int DiagnosticTool_exec()
+int DiagnosticTool_exec(void* obj)
 {
     return 0;
 }
 
-int MessageBox_exec()
+int MessageBox_exec(void* obj)
 {
     return 1;
+}
+
+void QSystemTrayIcon_show()
+{
+    return;
+}
+
+void MainFrame_onDownloadNewUrl(QString url, QString savePath, QString fileName, QString type)
+{
+    return;
+}
+
+bool MainFrame_showRedownloadMsgbox(const QString sameUrl)
+{
+    return true;
+}
+
+bool QIODevice_open(QIODevice::OpenMode flags)
+{
+    return true;
 }
