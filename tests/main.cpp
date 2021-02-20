e3a2abe097bf9c3e29c43c9f8c5e5f95b8b37d74
zhaoyue <zhaoyue@uniontech.com>
2021-02-20 09:45:20 +0800

    test
    Change-Id: I44ba42517395703dfe01ad43c839bd3682296f0d

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,14 @@
 #include "gtest/gtest.h"
 #include <QApplication>
-
+#include <QDir>
+#include <iostream>
 int main(int argc, char *argv[])
 {
+    std::cout << "---------------------------------------" << std::endl;
+    std::cout << QDir::current().absolutePath().toStdString() << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
     qputenv("QT_QPA_PLATFORM", "offscreen");
     QApplication a(argc, argv);
-
     testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
 
