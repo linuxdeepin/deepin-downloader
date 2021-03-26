7a8a4e7d1184f04a6b6f4dd7b72c86dcb44120a4
bulongwei@uniontech.com <bulongwei@uniontech.com>
2021-03-26 16:54:48 +0800

    fix: 增加单元测试
    增加单元测试
    
    Log: 增加单元测试
    Change-Id: If6c1f3ffc8a587930edf21352b06a4feabb7faa9

--- tests/ui/ut_messagebox.cpp
+++ tests/ui/ut_messagebox.cpp
@@ -4,6 +4,7 @@
 #include "func.h"
 #include "messagebox.h"
 #include <QObject>
+#include <QPushButton>
 class ut_messageBox : public ::testing::Test
 {
 protected:
@@ -45,18 +46,34 @@ TEST_F(ut_messageBox, onClearBtnClicked)
 {
     MessageBox msg;
     msg.onClearBtnClicked(0);
+    msg.onClearBtnClicked(1);
 }
 
 TEST_F(ut_messageBox, onDeleteBtnClicked)
 {
     MessageBox msg;
     msg.onDeleteBtnClicked(0);
+    msg.setDelete(true);
+    msg.m_DeleteFlag = true;
+    msg.onDeleteBtnClicked(1);
+    msg.m_DeleteFlag = false;
+    msg.onDeleteBtnClicked(1);
 }
 
 TEST_F(ut_messageBox, onExitBtnClicked)
 {
     MessageBox msg;
     msg.onExitBtnClicked(0);
+    msg.addCheckbox("11");
+    msg.addRadioGroup("11", "22");
+    msg.m_CheckBox->setCheckState(Qt::CheckState::Checked);
+    msg.m_ButtonMin->setChecked(true);
+    msg.onExitBtnClicked(1);
+    msg.m_CheckBox->setCheckState(Qt::CheckState::Unchecked);
+    msg.onExitBtnClicked(1);
+    msg.m_CheckBox->setCheckState(Qt::CheckState::Checked);
+    msg.m_ButtonMin->setChecked(false);
+    msg.onExitBtnClicked(1);
 }
 
 TEST_F(ut_messageBox, setWarings)
@@ -65,25 +82,50 @@ TEST_F(ut_messageBox, setWarings)
     msg.setWarings("warring", "warring");
 }
 
+TEST_F(ut_messageBox, setWarings1)
+{
+    MessageBox msg;
+    QStringList list;
+    list << "11" << "22";
+    msg.setWarings("warring", "warring", "warring",2,list);
+}
+
 TEST_F(ut_messageBox, setRedownload)
 {
     MessageBox msg;
     msg.setRedownload("");
+    msg.setRedownload("11",true,true);
 }
 
 TEST_F(ut_messageBox, setDelete)
 {
     MessageBox msg;
     msg.setDelete(true);
+    msg.setDelete(false);
 }
-//TEST_F(ut_messageBox, onRenamelineeditChanged)
-//{
-//    MessageBox msg;
-//    msg.onRenamelineeditChanged("text");
-//}
+TEST_F(ut_messageBox, onRenamelineeditChanged)
+{
+    MessageBox msg;
+    msg.m_RenameSureButton = new QPushButton();
+    msg.onRenamelineeditChanged("text.ddd");
+    msg.onRenamelineeditChanged("text");
+}
 
 TEST_F(ut_messageBox, setClear)
 {
     MessageBox msg;
     msg.setClear();
 }
+
+TEST_F(ut_messageBox, addCheckbox)
+{
+    MessageBox msg;
+    msg.addCheckbox("11", true);
+}
+
+TEST_F(ut_messageBox, setNetWorkError)
+{
+    MessageBox msg;
+    msg.setNetWorkError("11");
+}
+
