d28f51c88d5b4996a76c8fed459904c9163e1c51
bulongwei@uniontech.com <bulongwei@uniontech.com>
2021-04-27 11:09:28 +0800

    fix: 添加cmakelist变量定义
    添加cmakelist变量定义
    
    Log: 添加cmakelist变量定义
    Change-Id: If1c158fbac46122bc008121390ed456226a5d241

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,23 +5,29 @@
 #include "dbinstance.h"
 #include "settings.h"
 #include "dlmapplication.h"
+#include "config.h"
 
-#if defined(CMAKE_SAFETYTEST)
+#if defined(CMAKE_SAFETYTEST)   // || defined(CMAKE_SAFETYTEST  "CMAKE_SAFETYTEST_ARG_ON")
 #include <sanitizer/asan_interface.h>
 #endif
 
 int main(int argc, char *argv[])
 {
+
+    std::cout<<"hello"<<CMAKE_SAFETYTEST<<std::endl;
     Settings::getInstance()->initWidget();
 
     //qputenv("QT_QPA_PLATFORM", "offscreen");
     DlmApplication a(argc, argv);
     testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
+    if(CMAKE_SAFETYTEST == "CMAKE_SAFETYTEST_ARG_ON"){
+        __sanitizer_set_report_path("asan.log");
+    }
 
-#if defined(CMAKE_SAFETYTEST)
-    __sanitizer_set_report_path("asan.log");
-#endif
+//#if defined(CMAKE_SAFETYTEST)
+
+//#endif
 
     return ret;
 }
