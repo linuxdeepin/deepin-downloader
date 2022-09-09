fb80d30730eba5b1dfdf6d5515215e5599b2964a
caimengci <caimengci@uniontech.com>
2022-09-09 15:19:27 +0800

    feat: 修改deepin-downloader项目合规性整改
    1.合规整改自动添加注释导致编译错误
    
    Log: 合规性整改第三轮修改

--- src/include/ui/createTask/createtaskwidget.h
+++ src/include/ui/createTask/createtaskwidget.h
@@ -2,33 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-﻿/**
- * @copyright 2020-2020 Uniontech Technology Co., Ltd.
- *
- * @file newtaskwidget.h
- *
- * @brief 新建任务类
- *
- * @date 2020-06-09 10:52
- *
- * Author: bulongwei  <bulongwei@uniontech.com>
- *
- * Maintainer: bulongwei  <bulongwei@uniontech.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef NEWTASKWIDGET_H
 #define NEWTASKWIDGET_H
 
