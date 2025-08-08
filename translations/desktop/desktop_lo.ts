87b2d7e16cee16c52cebffc80693bc2d402b7307
JWWTSL <167396168+JWWTSL@users.noreply.github.com>
2025-08-08 12:05:51 +0800

    feat: Update translations for Lao locale (#104)
    log: Translation update

---
+++ translations/desktop/desktop_lo.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Deepin Downloader</source>
+        <translation>Deepin ດາວໂຫຼດ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>Downloader</source>
+        <translation>ໂປຣແກຣມດາວໂຫຼດ</translation>
+    </message>
+</context>
+</TS>
