06bc56bb95e3f9b73e1da5d1363602c115a710c5
JWWTSL <167396168+JWWTSL@users.noreply.github.com>
2025-08-21 16:32:24 +0800

    fix: update translations of lo (#107)
    log: translations error
    
    bug:https://pms.uniontech.com/bug-view-329121.html

--- translations/desktop/desktop_lo.ts
+++ translations/desktop/desktop_lo.ts
@@ -1,15 +1,17 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lo">
 <context>
     <name>desktop</name>
     <message>
         <location filename="Desktop Entry]Name" line="0"/>
         <source>Deepin Downloader</source>
-        <translation>Deepin ດາວໂຫຼດ</translation>
+        <translation>ເຄື່ອງດາວໂຫລດ</translation>
     </message>
     <message>
         <location filename="Desktop Entry]GenericName" line="0"/>
         <source>Downloader</source>
-        <translation>ໂປຣແກຣມດາວໂຫຼດ</translation>
+        <translation>ເຄື່ອງດາວໂຫລດ</translation>
     </message>
 </context>
 </TS>
